e79ba33b08c8b1f35ec4f8ddb6da640aad4ce85a
Daisie Huang <daisie@transposit.com>
2019-04-24 02:37:18 +0000

    Commit from Transposit IDE

--- src/save_timesheet.js
+++ src/save_timesheet.js
@@ -8,15 +8,24 @@
   var raw_repo = "timesheets";
   var raw_file = params.filename;
   console.log("raw file is " + raw_file);
-  var gitBlob = api.run("github.get_files_in_repo", {owner: owner.login, repo: raw_repo, path: raw_file, ref: "master"})[0];
-  var body = { committer: {name: owner[0].name, email: owner[0].email},
+  try {
+  	var gitBlob = api.run("github.get_files_in_repo", {owner: owner[0].login, repo: raw_repo, path: raw_file, ref: "master"})[0];
+    var body = { committer: {name: owner[0].name, email: owner[0].email},
                message: "timesheet",
                content: base64,
                sha: gitBlob.sha,
                branch: "master"
              };
-  //api.run("github.add_file_to_repo", {owner: params.owner, path: raw_file, repo: raw_repo, $body: body});
+    api.run("github.add_file_to_repo", {owner: params.owner, path: raw_file, repo: raw_repo, $body: body});
+  } catch(err) {
+    var body = { committer: {name: owner[0].name, email: owner[0].email},
+               message: "timesheet",
+               content: base64,
+               branch: "master"
+             };
+    api.run("github.add_file_to_repo", {owner: params.owner, path: raw_file, repo: raw_repo, $body: body});
+  }
   
-  return encoded_raw;
+  return params.content;
 }
 
