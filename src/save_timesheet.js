9dce3f375dd7fd98b4d64450faa29f7d4fae221d
Daisie Huang <daisie@transposit.com>
2019-04-24 02:27:25 +0000

    Commit from Transposit IDE

---
+++ src/save_timesheet.js
@@ -0,0 +1,22 @@
+(params) => {
+  var owner = api.run("github.get_user_authenticated");
+  console.log(owner);
+  var CryptoJS = require("crypto-js");
+
+  var wordArray = CryptoJS.enc.Utf8.parse(params.content);
+  var base64 = CryptoJS.enc.Base64.stringify(wordArray);
+  var raw_repo = "timesheets";
+  var raw_file = params.filename;
+  console.log("raw file is " + raw_file);
+  var gitBlob = api.run("github.get_files_in_repo", {owner: owner.login, repo: raw_repo, path: raw_file, ref: "master"})[0];
+  var body = { committer: {name: owner[0].name, email: owner[0].email},
+               message: "timesheet",
+               content: base64,
+               sha: gitBlob.sha,
+               branch: "master"
+             };
+  //api.run("github.add_file_to_repo", {owner: params.owner, path: raw_file, repo: raw_repo, $body: body});
+  
+  return encoded_raw;
+}
+
--- src/untitled.js
+++ src/untitled.js
@@ -1,19 +1,33 @@
 (params) => {
   var moment = require('moment.js');
   var report = api.run("this.get_detailed_report", {start_date: params.start_date, end_date: params.end_date});
-  var table = [];
+  var table = [['"Project Name"', '"Task Name"', '"Notes"', '"Time Spent"', '"Date"', '"Billable Status"', '"Staff Name"', '"Email"']];
   var total = 0;
   
   for (var i in report) {
     total += report[i].billable;
     var row = [];
+    row.push('"'+report[i].client+'"');
+    row.push('"'+report[i].project+'"');
+    row.push('"'+report[i].description+'"');
     var duration = moment.duration(report[i].dur);
-    report[i].dur = duration.hours() + ":" + duration.minutes();
-    report[i].start = moment(report[i].start).format('YYYY-MM-DD');
-    report[i].email = "daisieh@gmail.com"
+    row.push('"'+String(duration.hours() + ":" + duration.minutes())+'"');
+    row.push('"'+moment(report[i].start).format('YYYY-MM-DD')+'"');
+    var billable = "Non Billable";
+    if (report[i].is_billable) { billable = "Billable"; }
+    row.push('"'+billable+'"');
+    row.push('"'+report[i].user+'"');
+    row.push('"daisieh@gmail.com"');
+    table.push(row);
   }
-  console.log("total $" + total);
-  return report;
+  console.log("total $" + total.toFixed(2));
+  
+  var csv = "";
+  for (var i in table) {
+    csv += table[i].join() + "\n";
+  }
+  
+  api.run("this.save_timesheet", {filename: params.end_date+ ".csv", content: csv});
 }
 
 /*
