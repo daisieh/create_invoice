61c58a55b2899f669938f0ad5e86af247c3bcd8c
Daisie Huang <daisie@transposit.com>
2019-04-24 01:32:45 +0000

    Commit from Transposit IDE

--- src/untitled.js
+++ src/untitled.js
@@ -2,15 +2,17 @@
   var moment = require('moment.js');
   var report = api.run("this.get_detailed_report", {start_date: params.start_date, end_date: params.end_date});
   var table = [];
+  var total = 0;
   
   for (var i in report) {
+    total += report[i].billable;
     var row = [];
     var duration = moment.duration(report[i].dur);
     report[i].dur = duration.hours() + ":" + duration.minutes();
     report[i].start = moment(report[i].start).format('YYYY-MM-DD');
     report[i].email = "daisieh@gmail.com"
   }
-  
+  console.log("total $" + total);
   return report;
 }
 
