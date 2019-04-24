98230b6b07581c85978b489e0de4461e3320e79b
Daisie Huang <daisie@transposit.com>
2019-04-24 04:23:28 +0000

    Commit from Transposit IDE

--- src/untitled.js
+++ src/untitled.js
@@ -3,7 +3,6 @@
   var report = api.run("this.get_detailed_report", {start_date: params.start_date, end_date: params.end_date});
   var table = [['"Project Name"', '"Task Name"', '"Notes"', '"Time Spent"', '"Date"', '"Billable Status"', '"Staff Name"', '"Email"']];
   var total = 0;
-  
   for (var i in report) {
     total += report[i].billable;
     var row = [];
@@ -11,7 +10,11 @@
     row.push('"'+report[i].project+'"');
     row.push('"'+report[i].description+'"');
     var duration = moment.duration(report[i].dur);
-    row.push('"'+String(duration.hours() + ":" + duration.minutes())+'"');
+    var min = duration.minutes();
+    if (String(min).length < 2) {
+      min = "0" + String(min);
+    }
+    row.push('"'+String(duration.hours() + ":" + min)+'"');
     row.push('"'+moment(report[i].start).format('YYYY-MM-DD')+'"');
     var billable = "Non Billable";
     if (report[i].is_billable) { billable = "Billable"; }
